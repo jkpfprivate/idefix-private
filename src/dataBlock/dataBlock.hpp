d8bfcc78c294cfb85c3e817e2336579450248d09
kempf <jean.kempf@irap.omp.eu>
2023-07-30 14:17:25 +0200

    adding a vsh object to datablock class when VSH is enabled

--- src/dataBlock/dataBlock.hpp
+++ src/dataBlock/dataBlock.hpp
@@ -19,6 +19,9 @@
 #include "gridHost.hpp"
 #include "planetarySystem.hpp"
 #include "gravity.hpp"
+#if VSH == YES
+  #include "vsh.hpp"
+#endif // VSH == YES
 #include "stateContainer.hpp"
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
@@ -43,6 +46,9 @@ class Fluid;
 class SubGrid;
 class Vtk;
 class Dump;
+#if VSH == YES
+  class Vsh;
+#endif // VSH == YES
 
 // Forward class hydro declaration
 #include "physics.hpp"
@@ -162,6 +168,12 @@ class DataBlock {
   // Do we have Gravity ?
   bool haveGravity{false};
   std::unique_ptr<Gravity> gravity;
+
+  // Do we have VSH ?
+  #if VSH == YES
+//    std::unique_ptr<Vsh> vsh;
+    Vsh vsh;
+  #endif // VSH == YES
 
   // User step functions (before or after the main integrator step)
   void LaunchUserStepFirst();     ///< perform user-defined step before main integration step
