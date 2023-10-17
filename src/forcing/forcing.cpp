e05477fabf68c54504cd4f6ed63e2e6365b60287
kempf <jean.kempf@irap.omp.eu>
2023-10-17 14:36:07 +0200

    cleaning the code and add error message handling ell, m in forcing

--- src/forcing/forcing.cpp
+++ src/forcing/forcing.cpp
@@ -17,16 +17,19 @@ Forcing::Forcing(Input &input, DataBlock *datain) {
   this->seed = input.GetOrSet<int>("Forcing","seed",0,0);
 
   // Forcing
-  #if GEOMETRY == SPHERICAL
+  #if GEOMETRY == SPHERICAL & VSH == YES
     this->lmin = input.GetOrSet<int>("Forcing","lmin",0, 0);
     this->mmin = input.GetOrSet<int>("Forcing","mmin",0, 0);
     this->lmax = input.GetOrSet<int>("Forcing","lmax",0, 3);
     this->mmax = input.GetOrSet<int>("Forcing","mmax",0, 3);
     if (mmax > lmax) {
-      IDEFIX_ERROR("mmax cannot be greater than lmax");
-    }
-    if (mmin > lmin) {
-      IDEFIX_ERROR("mmin cannot be greater than lmin");
+      IDEFIX_ERROR("Forcing: mmax cannot be greater than lmax");
+    } else if (mmin > lmin) {
+      IDEFIX_ERROR("Forcing: mmin cannot be greater than lmin");
+    } else if (lmin >= lmax) {
+      IDEFIX_ERROR("Forcing: lmax cannot be less than or equal to lmax");
+    } else if (mmin >= mmax) {
+      IDEFIX_ERROR("Forcing: mmax cannot be less than or equal to mmax");
     }
     this->t_corr = input.GetOrSet<real>("Forcing","t_corr",0, 1.);
     this->eps_Ylm = input.GetOrSet<real>("Forcing","eps_Ylm",0, 1.);
@@ -38,7 +41,7 @@ Forcing::Forcing(Input &input, DataBlock *datain) {
                                 data->np_tot[KDIR], data->np_tot[JDIR], data->np_tot[IDIR]);
   
     this->OUprocesses.InitProcesses(this->seed,this->lmin,this->lmax,this->mmin,this->mmax,0.,t_corr,eps_Ylm,eps_Slm,eps_Tlm);
-  #endif // GEOMETRY == SPHERICAL
+  #endif // GEOMETRY == SPHERICAL & VSH = YES
 
 //  this->skipGravity = input.GetOrSet<int>("Gravity","skip",0,1);
 //  if(skipGravity<1) {
@@ -49,11 +52,13 @@ Forcing::Forcing(Input &input, DataBlock *datain) {
 
 void Forcing::ShowConfig() {
   #if GEOMETRY == SPHERICAL
-    idfx::cout << "Forcing: ENABLED with seed " << seed << "." << std::endl;
-    idfx::cout << "Forcing: lmin=" << lmin << " and lmax=" << lmax << "." << std::endl;
-    idfx::cout << "Forcing: mmin=" << mmin << " and mmax=" << mmax << "." << std::endl;
-    idfx::cout << "Forcing: t_corr=" << t_corr << " ." << std::endl;
-    idfx::cout << "Forcing: eps_Ylm=" << eps_Ylm << ", eps_Slm=" << eps_Slm << ", eps_Tlm =" << eps_Tlm << " ." << std::endl;
+    #if VSH == YES
+      idfx::cout << "Forcing: ENABLED with seed " << seed << "." << std::endl;
+      idfx::cout << "Forcing: lmin=" << lmin << " and lmax=" << lmax << "." << std::endl;
+      idfx::cout << "Forcing: mmin=" << mmin << " and mmax=" << mmax << "." << std::endl;
+      idfx::cout << "Forcing: t_corr=" << t_corr << " ." << std::endl;
+      idfx::cout << "Forcing: eps_Ylm=" << eps_Ylm << ", eps_Slm=" << eps_Slm << ", eps_Tlm=" << eps_Tlm << " ." << std::endl;
+    #endif // VSH == YES
   #endif // GEOMETRY == SPHERICAL
 //    if(skipGravity>1) {
 //      idfx::cout << "Gravity: gravity field will be updated every " << skipGravity
@@ -79,37 +84,39 @@ void Forcing::ComputeForcing(real dt) {
     IDEFIX_ERROR("Forcing in CYLINDRICAL geometry not implemented");
   #endif // GEOMETRY == CYLINDRICAL
   #if GEOMETRY == SPHERICAL
-    int lmin = this->lmin;
-    int lmax = this->lmax;
-    int mmin = this->mmin;
-    int mmax = this->mmax;
-    IdefixArray2D<real> jl = this->data->jl;
-    IdefixArray4D<real> Ylm_r = this->data->Ylm_r;
-    IdefixArray4D<real> Slm_th = this->data->Slm_th;
-    IdefixArray4D<real> Slm_phi = this->data->Slm_phi;
-    IdefixArray4D<real> Tlm_th = this->data->Tlm_th;
-    IdefixArray4D<real> Tlm_phi = this->data->Tlm_phi;
-    IdefixArray4D<real> forcingTerm = this->forcingTerm;
-    IdefixArray3D<real> OuValues = this->OUprocesses.ouValues;
-    idefix_for("ComputeForcing", 0, data->np_tot[KDIR], 0, data->np_tot[JDIR], 0, data->np_tot[IDIR],
-                KOKKOS_LAMBDA (int k, int j, int i) {
-                    real forcing_MX1=0;
-                    real forcing_MX2=0;
-                    real forcing_MX3=0;
-                    for (int l=lmin; l<lmax; l++) {
-//                      real jli = jl(l,i);
-                      real jli = 1.;
-                      for (int m=mmin; m<mmax; m++) {
-                        forcing_MX1 += jli*Ylm_r(l,m,k,j)*OuValues(0,l,m);
-                        forcing_MX2 += jli*Slm_th(l,m,k,j)*OuValues(1,l,m) + jli*Tlm_th(l,m,k,j)*OuValues(2,l,m);
-                        forcing_MX3 += jli*Slm_phi(l,m,k,j)*OuValues(1,l,m) + jli*Tlm_phi(l,m,k,j)*OuValues(2,l,m);
+    #if VSH == YES
+      int lmin = this->lmin;
+      int lmax = this->lmax;
+      int mmin = this->mmin;
+      int mmax = this->mmax;
+      IdefixArray2D<real> jl = this->data->jl;
+      IdefixArray4D<real> Ylm_r = this->data->Ylm_r;
+      IdefixArray4D<real> Slm_th = this->data->Slm_th;
+      IdefixArray4D<real> Slm_phi = this->data->Slm_phi;
+      IdefixArray4D<real> Tlm_th = this->data->Tlm_th;
+      IdefixArray4D<real> Tlm_phi = this->data->Tlm_phi;
+      IdefixArray4D<real> forcingTerm = this->forcingTerm;
+      IdefixArray3D<real> OuValues = this->OUprocesses.ouValues;
+      idefix_for("ComputeForcing", 0, data->np_tot[KDIR], 0, data->np_tot[JDIR], 0, data->np_tot[IDIR],
+                  KOKKOS_LAMBDA (int k, int j, int i) {
+                      real forcing_MX1=0;
+                      real forcing_MX2=0;
+                      real forcing_MX3=0;
+                      for (int l=lmin; l<lmax; l++) {
+//                        real jli = jl(l,i);
+                        real jli = 1.;
+                        for (int m=mmin; m<mmax; m++) {
+                          forcing_MX1 += jli*Ylm_r(l,m,k,j)*OuValues(0,l,m);
+                          forcing_MX2 += jli*Slm_th(l,m,k,j)*OuValues(1,l,m) + jli*Tlm_th(l,m,k,j)*OuValues(2,l,m);
+                          forcing_MX3 += jli*Slm_phi(l,m,k,j)*OuValues(1,l,m) + jli*Tlm_phi(l,m,k,j)*OuValues(2,l,m);
+                        }
                       }
-                    }
    
-                    forcingTerm(IDIR,k,j,i) += forcing_MX1;
-                    forcingTerm(JDIR,k,j,i) += forcing_MX2;
-                    forcingTerm(KDIR,k,j,i) += forcing_MX3;
-    });
+                      forcingTerm(IDIR,k,j,i) += forcing_MX1;
+                      forcingTerm(JDIR,k,j,i) += forcing_MX2;
+                      forcingTerm(KDIR,k,j,i) += forcing_MX3;
+      });
+    #endif // VSH == YES
   #endif // GEOMETRY == SPHERICAL
 
   OUprocesses.UpdateProcessesValues(dt);
