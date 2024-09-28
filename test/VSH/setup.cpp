c989af6b9aac6f71b88b1226f8ef86176d6ec6b3
kempf <jean.kempf@irap.omp.eu>
2024-09-28 20:25:20 +0200

    made the complex Vsh pass a regression test, but the vsh scripts are a real mess for now

--- test/VSH/setup.cpp
+++ test/VSH/setup.cpp
@@ -1,10 +1,5 @@
 #include "idefix.hpp"
 #include "setup.hpp"
-#include <vector>
-#include <fstream>
-#include <random>
-#include <cmath>
-
 
 // Initialisation routine. Can be used to allocate Arrays or variables which are used later on
 Setup::Setup(Input &input, Grid &grid, DataBlock &data, Output &output) {
@@ -15,65 +10,56 @@ Setup::Setup(Input &input, Grid &grid, DataBlock &data, Output &output) {
 // One can therefore define locally
 // a datahost and sync it, if needed
 void Setup::InitFlow(DataBlock &data) {
-  // Create a host copy
-//  DataBlockHost d(data);
+    // Create a host copy
   DataBlockHost d(data, 1);
- 
-  // Shallow copy the global array
-  for(int k = 0; k < d.np_tot[KDIR] ; k++) {
-    for(int j = 0; j < d.np_tot[JDIR] ; j++) {
-      for(int i = 0; i < d.np_tot[IDIR] ; i++) {
+//  d.SyncToDevice();
+//  std::default_random_engine generator;
 
+  int countr = 0;
+  int countth = 0;
+  int countphi = 0;
+  for(int k = 2; k < d.np_tot[KDIR]-2 ; k++) {
+    for(int j = 2; j < d.np_tot[JDIR]-2 ; j++) {
+      for(int i = 2; i < d.np_tot[IDIR]-2 ; i++) {
         real x1=d.x[IDIR](i);
         real x2=d.x[JDIR](j);
         real x3=d.x[KDIR](k);
         d.Vc(RHO,k,j,i) = 1.;
         d.Vc(PRS,k,j,i) = 1.;
-
-//        d.Vc(VX1,k,j,i) = ZERO_F;
-//          d.Vc(VX2,k,j,i) = 1.;
-//        d.Vc(VX2,k,j,i) = ZERO_F;
-//        d.Vc(VX3,k,j,i) = ZERO_F;
-//        d.Vc(VX1,k,j,i) = vsh.Ylm_r(1,0,k,j);
-//        d.Vc(VX1,k,j,i) = vsh.Ylm_r(1,0,k,j)/pow(x1,2.);
-//        d.Vc(VX1,k,j,i) = d.vsh->Ylm_r(1,0,k,j);
-//        d.Vc(VX2,k,j,i) = d.vsh->Slm_th(1,1,k,j);
-//        d.Vc(VX3,k,j,i) = d.vsh->Slm_phi(1,1,k,j);
-//        d.Vc(VX1,k,j,i) = ;
-//        d.Vc(VX2,k,j,i) = d.vsh->Slm_th(1,1,k,j);
-//        d.Vc(VX3,k,j,i) = d.vsh->Slm_phi(1,1,k,j);
-//        d.Vc(VX1,k,j,i) = normal_distrib(generator);
-//        d.Vc(VX2,k,j,i) = normal_distrib(generator);
-//        d.Vc(VX3,k,j,i) = normal_distrib(generator);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-d.Vc(VX1,k,j,i) = d.Ylm_r(3,3,k,j);
-d.Vc(VX2,k,j,i) = d.Tlm_th(3,3,k,j);
-d.Vc(VX3,k,j,i) = d.Tlm_phi(3,3,k,j);
-
-//          d.Vs(BX1s,k,j,i) = ZERO_F;
-//          d.Vs(BX2s,k,j,i) = d.vsh->Tlm_ths(2,2,k,j);
-//          d.Vs(BX3s,k,j,i) = d.vsh->Tlm_phis(2,2,k,j); 
+        
+//        d.Vc(VX1,k,j,i) = data.Ylm_r(2,2,k,j);
+//        d.Vc(VX2,k,j,i) = data.Tlm_th(1,1,k,j);
+//        d.Vc(VX3,k,j,i) = data.Tlm_phi(1,1,k,j);
+        d.Vc(VX1,k,j,i) = d.Ylm_r(2,2,k,j).real();
+        d.Vc(VX2,k,j,i) = d.Tlm_th(1,1,k,j).real();
+        d.Vc(VX3,k,j,i) = d.Tlm_phi(1,1,k,j).real();
+        if (FABS(1.*d.Vc(VX3,k,j,i) - sqrt(2.*3./(8.*M_PI))*COS(x2)*COS(x3)) > 1e-6) {
+          countphi ++;
+//std::cout << d.Vc(VX3,k,j,i)/sqrt(2.*3./(8.*M_PI))*COS(x2)*COS(x3) << std::endl;
+//std::cout << d.Tlm_phi(1,1,k,j).imag()/sqrt(2.*3./(8.*M_PI))*COS(x2)*COS(x3) << std::endl;
+////std::cout << d.Vc(VX3,k,j,i) << std::endl;
+//std::cout << d.Tlm_phi(1,1,k,j) << std::endl;
+//std::cout << std::endl;
+std::cout << d.Tlm_phi(1,1,k,j)/(sqrt(2.*3./(8.*M_PI))*COS(x2)*COS(x3)) << std::endl;
+        }
+        if (FABS(1.*d.Vc(VX2,k,j,i) - sqrt(2.*3./(8.*M_PI))*SIN(x3)) > 1e-6) {
+          countth ++;
+//std::cout << d.Tlm_th(1,1,k,j)/(sqrt(2.*3./(8.*M_PI))*SIN(x3)) << std::endl;
+        }
+        if (FABS(1.*d.Vc(VX1,k,j,i) - 0.5*sqrt(15./(2.*M_PI))*pow(SIN(x2),2)*COS(2.*x3)) > 1e-6) {
+          countr ++;
+std::cout << 2.*d.Vc(VX1,k,j,i)/(0.5*sqrt(15./(2.*M_PI))*pow(SIN(x2),2)*COS(2.*x3)) << std::endl;
+std::cout << d.Ylm_r(2,2,k,j) << std::endl;
+        }
       }
     }
   } 
+  if (countphi==0 & countth==0 & countr==0) {
+    std::cout << "SUCCESS: no discrepancy has been found." << std::endl;
+  } else {
+    std::cout << "FAILURE: countphi=" << countphi << ", countth=" << countth << " and countr=" << countr << std::endl;
+    IDEFIX_ERROR("Error");
+  }
   d.SyncToDevice();
 }
 
